8f04e3349e3f6579816c28a7758a52c610f3e635
IvanaCliffords <ivana.cliffords>
2022-05-25 15:13:12 -0500

    prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
+import path from 'path';
+
 import bodyParser from 'body-parser'
 import mongoose from "mongoose";
 import cors from 'cors';
 import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
-
 const app = express();
 dotenv.config();
 
@@ -13,7 +14,9 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/frontend/build/index.html')));
 const MONGODB_URI = process.env.MONGODB_URI;
 
 const PORT = process.env.PORT || 5001;
